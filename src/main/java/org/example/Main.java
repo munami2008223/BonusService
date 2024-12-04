8e3b1cadc40d6cbde8cef003b1907fd1b442d3cf
Natali <munami200823@yandex.ru>
2024-12-04 23:16:00 +0500

    Change2 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,34 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        BonusService service = new BonusService();
-
-        long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
-        long actual = service.calculate(amount, registered);
-
-        System.out.println(actual);
-
-        amount = 1_000_000;
-        registered = true;
-        expected = 500;
-        actual = service.calculate(amount, registered);
-
-        System.out.println(actual);
-
-        amount = 1000;
-        registered = false;
-        expected = 10;
-        actual = service.calculate(amount, registered);
-
-        System.out.println(actual);
-
-        amount = 1_000_000;
-        registered = false;
-        expected = 500;
-        actual = service.calculate(amount, registered);
-
-        System.out.println(actual);
     }
 }
