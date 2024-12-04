b8be235e8dcb1acad78f42f47cb0004191ab8951
Natali <munami200823@yandex.ru>
2024-12-04 20:44:05 +0500

    Inicial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+
+        System.out.println(actual);
+
+        amount = 1_000_000;
+        registered = true;
+        expected = 500;
+        actual = service.calculate(amount, registered);
+
+        System.out.println(actual);
+
+        amount = 1000;
+        registered = false;
+        expected = 30;
+        actual = service.calculate(amount, registered);
+
+        System.out.println(actual);
+
+        amount = 1_000_000;
+        registered = false;
+        expected = 500;
+        actual = service.calculate(amount, registered);
+
+        System.out.println(actual);
+    }
+}
